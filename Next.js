5a134784d68ade394bc391bdc5e7e91215c4f14a
Austin Gae <austingae@gmail.com>
2022-07-09 19:50:07 -0700

    Created the Next.js file, in which I explain the difference between getStaticProps() and getServerSideProps()

---
+++ Next.js
@@ -0,0 +1,13 @@
+/*
+getStaticProps will get the data at only build time. 
+Therefore, if I add new data, then the website page will not show the new data.
+To show the new data, I will need to do build again.
+CORRECT!
+
+
+
+getServerSideProps will get the data before the website page loads. 
+Therefore, if I add new data, and I refresh the website page, then the website page will
+show the new data. 
+CORRECT!
+*/
