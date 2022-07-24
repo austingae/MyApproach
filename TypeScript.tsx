a761873549b8035428048964381d0d49ad874d61
Austin Gae <austingae@gmail.com>
2022-07-24 15:29:02 -0700

    TypeScript.tsx

---
+++ TypeScript.tsx
@@ -0,0 +1,6 @@
+/*
+TypeScript: A Static Type Checker
+TypeScript is a static type checker, which means that it detects errors before runtime based on types. 
+
+EveryDay Types: https://www.typescriptlang.org/docs/handbook/2/everyday-types.html
+*/
